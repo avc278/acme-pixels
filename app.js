26b33f529fbbd52bb5c3c8634124712f9b328651
Andres Callanaupa <avc278@nyu.edu>
2020-01-26 10:50:11 -0500

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const hex = [
+    '0', '3', '6', '9', 'c', 'f'
+]
+
+const div = document.querySelector('div');
+div.addEventListener('click', () => {
+    ev.target.style.backgroundColor = '';
+    ev.target.style.borderWidth = '1px';
+});
